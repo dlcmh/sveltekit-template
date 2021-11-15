b05a5bd38eccf6c4acdc88c14448fa4bb1f265a9
David Chin <dlcmhd@me.com>
2021-11-15 15:16:15 +0800

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Footer from '$lib/components/Footer.svelte';
+  import Nav from '$lib/components/navigations/Nav.svelte';
+</script>
+
+<div class="container">
+  <section>
+    <Nav
+      links={[
+        { href: '/', title: 'Home' },
+        { href: '/sandbox', title: 'Sandbox' },
+      ]}
+    />
+    <main>
+      <slot />
+    </main>
+    <Footer />
+  </section>
+</div>
+
+<!-- https://kiranworkspace.com/how-to-stick-footer-to-bottom-of-page/ -->
+<style>
+  /* with display: flex */
+  /* section {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  main {
+    flex: 1;
+  } */
+
+  /* with display: grid */
+  section {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+  }
+</style>
